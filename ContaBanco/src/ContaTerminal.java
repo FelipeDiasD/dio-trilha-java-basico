9964a76964738f5b8e1812e8a37e550050690eb6
Felipe <prado.dias@hotmail.com>
2024-05-02 22:47:07 -0300

    feat: utilização de wrapper class e finalização do desafio com bufferedReader

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -10,11 +10,29 @@ public class ContaTerminal {
         BufferedReader reader = 
           new BufferedReader(new InputStreamReader(System.in));
 
-          int numero;
+          Integer numero = 0;
           String agencia;
           String nomeCliente;
-          float saldo;
+          Float saldo = (float) 0;
     
-        System.out.println("Hello, World!");
+        System.out.print("Insira o numero de sua conta: ");
+        numero = numero.parseInt(reader.readLine());
+
+        System.out.print("Por favor, insira o numero de sua agencia: ");
+        agencia = reader.readLine();
+        
+        System.out.print("Agora, confirme seu nome completo inserindo-o a seguir: ");
+        nomeCliente = reader.readLine();
+
+        //Não faz muito sentido o usuário inserir o saldo mas ok, tá pedindo kk
+        System.out.print("Por ultimo, insira o ultimo saldo conhecido de sua conta: ");
+        saldo = saldo.parseFloat(reader.readLine());
+
+        
+
+        System.out.println
+        ("Ola " +  nomeCliente + " obrigado por criar uma conta em nosso banco, sua agencia e " +
+        agencia + " numero " + numero + " e seu saldo de R$" + saldo +" ja esta disponivel!");
+        
     }
 }
